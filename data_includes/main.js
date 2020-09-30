e121091ea4733ff5b78d7fff6a53cf23616a8f50
joschwalee17 <46580409+joschwalee17@users.noreply.github.com>
2020-09-30 20:24:46 +0900

    Add files via upload

--- data_includes/main.js
+++ data_includes/main.js
@@ -1,3 +1,61 @@
 PennController.ResetPrefix(null); // Initiates PennController
 
-// Start typing your code here
+PennController.ResetPrefix(null)
+Sequence( "welcome" , randomize("experiment") , "send" , "final" )
+newTrial( "welcome" ,
+    defaultText
+        .print()
+    ,
+    newText("<p>Welcome!</p>")
+    ,
+    newText("<p>In this experiment, you will report the direction of the arrow in the middle.</p>")
+    ,
+    newText("<p>Press the <strong>F</strong> key if the middle arrow is pointing left, and the <strong>J</strong> key if the middle arrow is pointing right.</p>")
+    ,
+    newText("<p>Remember to only pay attention to the arrow in the middle.</p>")
+    ,
+    newText("<p>Please enter your ID and then click the button below to start the experiment.</p>")
+    ,
+    newTextInput("inputID")
+        .print()
+    ,
+    newButton("Start")
+        .print()
+        .wait()
+    ,
+    newVar("ID")
+        .global()
+        .set( getTextInput("inputID") )
+)
+.log( "ID" , getVar("ID") )
+Template( variable => 
+  newTrial( "experiment" ,
+    newTimer(500)
+        .start()
+        .wait()
+    ,
+    newImage("arrow", variable.arrow)
+        .size(200,200)
+        .print()
+    ,
+    newSelector()
+        .keys("F", "J")
+        .log()
+        .wait()
+    ,
+    newTimer(500)
+        .start()
+        .wait()
+  )
+  .log( "ID"     , getVar("ID")    )
+  .log( "Middle Direction"   , variable.MidDir   )
+  .log( "Surrounding Direction" , variable.SurDir )
+)
+SendResults( "send" )
+newTrial( "final" ,
+    newText("<p>Thank you for your participation!</p>")
+        .print()
+    ,
+    newButton("void")
+        .wait()
+
